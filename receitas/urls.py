61dac36bda250f7f64f96e8598bffd9c473b501c
gvilkson <gvilkson@gmail.com>
2022-03-15 13:10:34 -0400

    Criando rota e view para página de receita. Atualizado links home e receita.

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'receitas'
-
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('receita/', views.receita, name='receita'),
 ]
