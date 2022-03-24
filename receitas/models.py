622f9409234eb6508d6d398a5f4a2a5080cae107
gvilkson <gvilkson@gmail.com>
2022-03-23 22:42:31 -0400

    Integrando modelos

--- receitas/models.py
+++ receitas/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 
 # Create your models here.
 class Receita(models.Model):
+	pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
 	nome_receita = models.CharField('Nome da receita', max_length=200)
 	ingredientes = models.TextField('Ingredientes')
 	modo_preparo = models.TextField('Modo de preparo')
