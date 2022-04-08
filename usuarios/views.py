0769432e01ca2b558c748348e2eceddf31367d87
gvilkson <gvilkson@gmail.com>
2022-04-08 11:12:18 -0400

    CSRF, token e dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def cadastro(request):
-	return render(request, 'usuarios/cadastro.html')
+	if request.method == 'POST':
+		nome = request.POST['nome']
+		email = request.POST['email']
+		senha = request.POST['password']
+		senha2 = request.POST['password2']
+		print(f'{nome}\n{email}\n{senha}\n{senha2}')
+		return redirect('login')
+	else:
+		return render(request, 'usuarios/cadastro.html')
 
 def login(request):
 	return render(request, 'usuarios/login.html')
@@ -11,4 +19,3 @@ def logout(request):
 
 def dashboard(request):
 	pass
-
