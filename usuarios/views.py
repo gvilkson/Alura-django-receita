fa3ea5bb75d83ebeebab54616eae7d2e664c77f2
gvilkson <gvilkson@gmail.com>
2022-04-09 09:31:49 -0400

    Criando usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
 	if request.method == 'POST':
@@ -6,7 +7,27 @@ def cadastro(request):
 		email = request.POST['email']
 		senha = request.POST['password']
 		senha2 = request.POST['password2']
-		print(f'{nome}\n{email}\n{senha}\n{senha2}')
+
+		if not nome.strip():
+			print('O campo nome não pode ficar em branco!')
+			return redirect('cadastro')
+
+		if not email.strip():
+			print('O campo email não pode ficar em branco!')
+			return redirect('cadastro')
+
+		if not senha.strip():
+			print('O campo senha não pode ficar em branco!')
+			return redirect('cadastro')
+
+		if User.objects.filter(email=email).exists():
+			print('Usuário já cadastrado no sistema!')
+			return redirect('cadastro')
+
+		user = User.objects.create_user(username=nome, email=email, password=senha)
+		user.save()
+
+		print(f'Usuário:{nome} cadastrado com sucesso')
 		return redirect('login')
 	else:
 		return render(request, 'usuarios/cadastro.html')
